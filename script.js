4a48b6f136386a260126b3ee4ad01921bdb46906
Stefan-smiles <s.cookesec@gmail.com>
2024-10-13 15:35:10 +0100

    time for handin

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ const imgSelector = document.getElementById("image-selector");
 const imgBackground = document.getElementById("image-background");
 
 //interactivity(left right arrow keys)
+imgIndex = 0;
 
 const images = [
-  { src: "./angelic-pictures/angel-1.jpg", alt: "angel 1" },
-  { src: "./angelic-pictures/angel-2.jpg", alt: "angel 2" },
-  { src: "./angelic-pictures/angel-3.jpg", alt: "angel 3" },
-  { src: "./angelic-pictures/angel-4.jpg", alt: "angel 4" },
-  { src: "./angelic-pictures/angel-5.jpg", alt: "angel 5" },
-  { src: "./angelic-pictures/angel-6.jpg", alt: "angel 6" },
+  {
+    src: "./angelic-pictures/angel-1 100.jpg",
+    alt: "an image of an angel set on a corner piece  in stone.",
+  },
+  {
+    src: "./angelic-pictures/angel-3 100.jpg",
+    alt: "a rennaisance  style painting of angels amongst the clouds",
+  },
+  {
+    src: "./angelic-pictures/angel-4 100.jpg",
+    alt: "a stone angel stautue in a public space",
+  },
+  {
+    src: "./angelic-pictures/angel-5 100.jpg",
+    alt: "a sculpture of a warrior angel seemingly in a battle",
+  },
+  {
+    src: "./angelic-pictures/angel-6 100.jpg",
+    alt: "an image of an angel in tarot card style",
+  },
 ];
 
 for (let i = 0; i < images.length; i++) {
@@ -21,6 +36,13 @@ for (let i = 0; i < images.length; i++) {
     imgBackground.src = images[i].src;
     imgBackground.alt = images[i].alt;
     imgBackground.title = images[i].alt;
+  });
+
+  img.addEventListener("mouseover", function (e) {
+    e.target.style.border = "solid red 10px";
+  });
+  img.addEventListener("mouseout", function (e) {
+    e.target.style.border = "0px ";
   });
 
   // add a click event listener to the image
